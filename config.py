20ef4e916c30fd4240dab69f84af0ceede596dff
zoirxon0904 <e401023157@st.tachibana-u.ac.jp>
2026-04-22 23:46:33 +0500

    Update config.py

--- config.py
+++ config.py
@@ -10,24 +10,24 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 #    Kanal ID ni qanday topish: pastdagi README ga qarang
 REQUIRED_CHANNELS = [
     {
-        "id": -1003998098831,  # <-- kanal ID (manfiy son)
-        "link": "https://t.me/+WtepH9K9UKE3YzAy",
-        "name": "Kanal 3",
+        "id": -1002265625081,  # <-- kanal ID (manfiy son)
+        "link": "https://t.me/+WcdtRNyDSlE2MTkx",
+        "name": "𝑁𝑜𝑧𝑎 | 𝑁𝑒𝑤 𝑌𝑜𝑟𝑘",
     },
     {
-        "id": -1003979529915,  # <-- kanal ID
-        "link": "https://t.me/+CYFAMWL-IQI5MGNi",
-        "name": "Kanal 2",
+        "id": -1001989513167,  # <-- kanal ID
+        "link": "https://t.me/Dilbarkhon_Mansurovna",
+        "name": "Dilbar Mansurovna| Global Insights",
     },
     {
-        "id": -1003950616100,  # <-- kanal ID
-        "link": "https://t.me/+Nma3Ge6D_gJiYmIy",
-        "name": "Kanal 1",
+        "id": -1002568551776,  # <-- kanal ID
+        "link": "https://t.me/+b0_CsM93ckljMTA6",
+        "name": "Experiencia | by Dilbar",
     },
 ]
 
 # 3. Oxirida yuboriladigan kanal linki
-FINAL_CHANNEL_LINK = "https://t.me/Welcome_toHell_z"
+FINAL_CHANNEL_LINK = "https://t.me/+sn2GKZ-oZX9lM2Qy"
 
 # 4. Nechta do'st taklif qilish kerak
 REFERRAL_REQUIRED = 3
